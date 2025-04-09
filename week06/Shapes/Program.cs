6593d134f3ba192549bae457ba9519a7ba9904df
Rian Christani <rianchristani@hotmail.com>
2025-04-08 21:51:10 -0300

    w6-shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square sqr = new Square("Blue - Square", 15);
+        Console.WriteLine(sqr.GetColor());
+        Console.WriteLine(sqr.GetArea());
+
+        Rectangle rec = new Rectangle("Green - Rectangle", 10, 10);
+        Console.WriteLine(rec.GetColor());
+        Console.WriteLine(rec.GetArea());
+
+        Circle cir = new Circle("Purple - Circle", 5);
+        Console.WriteLine(cir.GetColor());
+        Console.WriteLine(cir.GetArea());        
+        
+        Console.WriteLine();
+
+        List<Shape>shapes = new List<Shape>();
+        shapes.Add(sqr);
+        shapes.Add(rec);
+        shapes.Add(cir);
+
+        foreach (Shape shape in shapes){
+        Console.WriteLine(shape.GetColor());
+        Console.WriteLine(shape.GetArea());        
+        }
     }
 }
